cdfb4182e14fcd127c586c47aff2819a1a6e1d9c
Amos Yuen <amosyuen@gmail.com>
2018-05-18 03:02:16 -0700

    Make it easier to run tests: - Set env vars in a cross platform compatible way - Prefer using knex to initialize the db so that the SQL db client doesn't need to be installed locally - Add setup file for databases using docker

--- test-api/data/schema/setup.js
+++ test-api/data/schema/setup.js
@@ -1,14 +1,11 @@
-const url = require('url')
-const { execSync } = require('child_process')
 const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
 const Promise = require('bluebird')
 
-module.exports = function(db, name) {
-
+module.exports = async function(db, name) {
   if (db === 'oracle') {
-    console.log('building oracle')
+    assert(process.env.ORACLE_URL, 'Must provide environment variable ORACLE_URL, e.g. "pass@localhost/"')
     const [ password, connectString ] = process.env.ORACLE_URL.split('@')
     const knex = require('knex')({
       client: 'oracledb',
@@ -20,82 +17,72 @@ module.exports = function(db, name) {
       }
     })
 
-    let schema = fs.readFileSync(path.join(__dirname, 'oracle.sql')).toString()
-    schema = schema.split(/\r?\n\r?\n/)
-    return Promise.mapSeries(schema.filter(i => i), stmt => {
-      console.log(stmt.trim())
-      return knex.raw(stmt.trim())
-    })
-    .then(() => knex)
-    .catch(err => {
-      console.error(err)
-      knex.destroy()
-      process.exit(1)
-    })
+    const schema = fs
+      .readFileSync(path.join(__dirname, 'oracle.sql'))
+      .toString()
+      .split(/\r?\n\r?\n/)
+      .map(stmt => stmt.trim())
+    await runStatements(knex, schema)
+    return knex
   }
 
   if (db === 'pg') {
     assert(process.env.PG_URL, 'Must provide environment variable PG_URL, e.g. "postgres://user:pass@localhost/"')
-    const out = execSync(`psql ${process.env.PG_URL + name} < ${__dirname}/pg.sql`)
-    if (out.toString()) {
-      console.log(out.toString())
-    }
-    return require('knex')({
+    const knex = require('knex')({
       client: 'pg',
       connection: process.env.PG_URL + name
     })
+    const schema = fs
+      .readFileSync(path.join(__dirname, 'pg.sql'))
+      .toString()
+      .split(/;/)
+    await runStatements(knex, schema)
+    return knex
   }
 
   if (db === 'mysql') {
     assert(process.env.MYSQL_URL, 'Must provide environment variable MYSQL_URL, e.g. "mysql://user:pass@localhost/"')
-    const args = parseConnectionStr(process.env.MYSQL_URL + name)
-    const out = execSync(`cat ${__dirname}/mysql.sql | mysql ${args}`)
-    if (out.toString()) {
-      console.log(out.toString())
-    }
-    return require('knex')({
+    const schema = fs
+      .readFileSync(path.join(__dirname, 'mysql.sql'))
+      .toString()
+      .split(/;/)
+    const knex = require('knex')({
       client: 'mysql',
       connection: process.env.MYSQL_URL + name
     })
+    await runStatements(knex, schema)
+    return knex
   }
 
   if (db === 'sqlite3') {
-    const out = execSync(`/bin/cat ${__dirname}/sqlite3.sql | sqlite3 ${__dirname}/../db/${name}-data.sl3`)
-    if (out.toString()) {
-      console.log(out.toString())
-    }
-
-    return require('knex')({
+    const knex = require('knex')({
       client: 'sqlite3',
       connection: {
         filename: __dirname + `/../db/${name}-data.sl3`
       },
       useNullAsDefault: true
     })
+    const schema = fs
+      .readFileSync(path.join(__dirname, 'sqlite3.sql'))
+      .toString()
+      .split(/;/)
+    await runStatements(knex, schema)
+    return knex
   }
 
   throw new Error(`do not recognize database "${db}"`)
 }
 
-function parseConnectionStr(dbUri) {
-  const parsed = url.parse(dbUri)
-  let args = ''
-  if (parsed.auth) {
-    const [ user, password ] = parsed.auth.split(':')
-    args += '--user=' + user
-    if (password) {
-      args += ' --password=' + password
-    }
+async function runStatements(knex, statements) {
+  try {
+    await Promise.mapSeries(statements.map(stmt => stmt.trim()).filter(stmt => stmt), stmt => {
+      console.log(stmt)
+      return knex.raw(stmt)
+    })
+    return knex
+  } catch (err) {
+    console.error(err)
+    knex.destroy()
+    process.exit(1)
   }
-  if (parsed.hostname) {
-    args += ' --host=' + parsed.hostname
-  }
-  if (parsed.port) {
-    args += ' --port=' + parsed.port
-  }
-  if (parsed.pathname) {
-    args += ` ${parsed.pathname.slice(1)}`
-  }
-  return args
 }
-
